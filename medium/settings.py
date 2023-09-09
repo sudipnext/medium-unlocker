b74b0182dba9ead3ab39396fabf6f4b7078b66b8
sudipnext <parajulisudip123@outlook.com>
2023-09-09 12:22:56 +0545

    Updated

--- medium/settings.py
+++ medium/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*xxgw(z=n06z!e)k8!(+tk7v4$=qu)9&3hgcmlm2tp-@bke)c2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
